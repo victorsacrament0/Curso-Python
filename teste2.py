648c3ca693f819eee910eecb9b166d9800576efe
VictorSacramento <victorscosta@gmail.com>
2026-03-20 10:50:06 -0300

    gitpush -u origin main

--- teste2.py
+++
@@ -1,9 +0,0 @@
-senha = input('Digite a senha: ')
-
-while senha != "1234":
-    print('Senha incorreta, tente novamente!')
-    senha = input('Digite a senha: ')
-if senha == "1234":
-        print('_' * 40)
-        print('Senha correta, acesso permitido!')
-        print('_' * 40)
