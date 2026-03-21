373f68fd8abfeb493271cfa3a13085d7b25d7a38
VictorSacramento <victorscosta@gmail.com>
2026-03-21 09:06:16 -0300

    gitpush main

--- repetiçãoexercicios.py
+++ repetiçãoexercicios.py
@@ -23,3 +23,17 @@ for preco in lista_preco:
     print(f'Valor do produto: {preco} com imposto de {imposto}')
 
 print('Total dos impostos é', total_imposto )
+
+
+#Usar o for num dicionario: calcular o percentual de crescimento
+#calculo valor / valor2 - 1 - procentagem de crescimento de um ano pro outro
+
+vendas23 = {'jan': 15000, 'fev':12000,'mar':13500}
+vendas24 = {'jan': 16000, 'fev':13000,'mar':12000}
+
+for mes in vendas24:
+    valor23 = vendas23[mes]
+    valor24 = vendas24[mes]
+    crescimento = valor24 / valor23 - 1
+    print(f'No mes de {mes} o crescimento foi de {crescimento:.1%}') 
+                                             #.1$ retorna quantas casas vc quer dps do . e % o tipo que deseja
