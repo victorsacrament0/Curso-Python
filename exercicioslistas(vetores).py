4f6e86e1f123e3d920f492b394f57579d2e5b1fd
VictorSacramento <victorscosta@gmail.com>
2026-04-06 20:40:54 -0300

    cmmt

--- exercicioslistas(vetores).py
+++ exercicioslistas(vetores).py
@@ -187,3 +187,17 @@ for linha in matriz:
 #soma = sum(sum(linha) for linha in numeros)
 #print(soma)
 
+#matriz = [
+#    [7, 10],
+#    [8, 2],
+#]
+ 
+#total = 0
+#ler cada item da lista
+#for linha in matriz:
+#   for itens in linha:
+#       total += itens
+# 
+#print(f"total: {total}")
+#
+#
