40934de401efefeef1255e555227d2835a8e8566
VictorSacramento <victorscosta@gmail.com>
2026-04-10 10:28:03 -0300

    cmmt

--- revisãovetores(lista).py
+++ revisãovetores(lista).py
@@ -76,3 +76,16 @@ print("Iten removido com pop", frutas)
 
 elemento = frutas.pop(2)
 print("Variavel elemento: ", elemento)
+
+##Tuplas
+
+segunda_tupla = (1,2,3)
+
+print(segunda_tupla[1:3])
+
+#Engamamos a tupla parte 3
+terceira_tupla = ("Dia", [25,32,0])
+print(f"Tupla original: {terceira_tupla}")
+
+terceira_tupla[1][2] = 45
+print(f"Tupla modificada: {terceira_tupla}")
