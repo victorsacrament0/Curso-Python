acb39f122998984fdf9bc10e98dd1eb75e0b278f
VictorSacramento <victorscosta@gmail.com>
2026-05-13 19:01:22 -0300

    aula02

---
+++ backend2026.1/aula02/fazercafe.py
@@ -0,0 +1,17 @@
+#função criada
+def fazer_cafe():
+    print('Café Pronto!')
+
+#como chamar a função
+
+fazer_cafe()
+
+
+""" crie uma funcao que mostre a mensagem """
+
+def msg():
+    print('Bem Vindo ao sistema!')
+
+msg()
+
+
