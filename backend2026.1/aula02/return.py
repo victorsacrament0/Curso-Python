77e0e19c5b284f6c90744d18d66ec01dabcf6818
VictorSacramento <victorscosta@gmail.com>
2026-05-13 21:53:56 -0300

    aula02

--- backend2026.1/aula02/return.py
+++ backend2026.1/aula02/return.py
@@ -17,11 +17,16 @@ def mult(num1,num2):
 print(mult(num1,num2))
 
 """ =========== """
-
-def desconto(num,porc):
-  return num * porc
-
-print(desconto(100,0.1))
+def desconto_10(valor_compra):
+    #Calcula qunto é 10% do valor da compra
+    desconto = valor_compra * 0.10
+    return valor_compra - desconto
+ 
+valor = int(input("Informe o valaor da compra: "))
+print("Aplicando cupom de 10%. Aguarde.. ")
+valor_desconto = desconto_10(valor)
+ 
+print(f"Desconto aplicado! Valor a pagar R${valor_desconto:.2f}")
 
 """ ========= """
 
