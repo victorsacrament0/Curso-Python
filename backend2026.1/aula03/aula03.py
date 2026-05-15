79c31bd4c87698d27328cf5c43d03ec2bddaf7cc
VictorSacramento <victorscosta@gmail.com>
2026-05-15 18:50:21 -0300

    aula3

--- backend2026.1/aula03/aula03.py
+++ backend2026.1/aula03/aula03.py
@@ -1,18 +1,18 @@
 valor1 = int(input('Digite um valor inteiro positivo: '))
 valor2 = int(input('Digite outro valor inteiro positivo: '))
-opm = input('Digite a operação matematica desejada: ')
+opm = input('Digite a operação matematica desejada: ').lower()
 
 def calc(valor1, valor2, opm):
-    if opm =='+':
+    if opm in ['+', 'mais', 'somar', 'soma']:
         resutado = valor1 + valor2
         return resutado
-    elif opm == '-':
+    elif opm in ['-', 'menos', 'subtrair']:
         resutado = valor1 - valor2
         return resutado
-    elif opm == 'x':
+    elif opm in ['x', 'multiplica', 'multiplicar']:
         resutado = valor1 * valor2
         return resutado
-    elif opm == '/':
+    elif opmin ['/', 'dividir', 'divisao', 'divisão']:
         if valor2 == 0:
            mensagem = "Não é possivel divisão por ZERO"
            return mensagem
