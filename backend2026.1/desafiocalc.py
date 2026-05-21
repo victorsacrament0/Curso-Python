b94a40dcd9c37fedfd54836347ca905258b6a07c
VictorSacramento <victorscosta@gmail.com>
2026-05-21 13:15:54 -0300

    cmmt

--- backend2026.1/desafiocalc.py
+++ backend2026.1/desafiocalc.py
@@ -1,6 +1,6 @@
 class Calculadora:
     def __init__(self):
-        print('|:.Calculadora Tres Esquilos.:|\n')
+        print('|:.Calculadora Três Esquilos.:|\n')
 
     def somar (self,nu1,nu2):
         print (f"\n|:Resultado: {nu1} + {nu2} = [{nu1 + nu2}]  :|\n")
