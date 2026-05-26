5bea8b1a71810b603740a676e9dc28e218d4017d
VictorSacramento <victorscosta@gmail.com>
2026-05-25 21:57:38 -0300

    aula07

--- backend2026.1/aula07/django/cliente/views.py
+++ backend2026.1/aula07/django/cliente/views.py
@@ -7,5 +7,5 @@ def hello(request):
     return HttpResponse('<h1>Olá Mundo!</h1>'  '<p>olá Django</p>')
 
 def contact(request):
-    return HttpResponse('<h2>Contato</h2><br><form><input type="email" placeholder="email@email.com.br"><br><input type=text placeholder="99 99999-9999"></form><br><button><b>Entrar</b></button>')
+    return HttpResponse('<h2>Contato</h2><br><form><input type="email" placeholder="email@email.com.br"><br><input type=text placeholder="99 99999-9999"></form><br><button><b>Enviar</b></button>')
 
