5d2d11a54c1e8dd26c7b78190c18bb9b486ec2e4
VictorSacramento <victorscosta@gmail.com>
2026-05-29 20:30:12 -0300

    aula09

--- XPTO/cadastro/views.py
+++ XPTO/cadastro/views.py
@@ -7,9 +7,9 @@ def dados_alunos (request):
     titulo = 'Nossos Alunos'
     
     lista_alunos = [
-        {'nome' : 'Thyago Matarazzo','curso' : 'Análise e Desenvolvimento de Sistemas','turma' : '2026.2Ma'},
+        {'nome' : 'Thyago Matarazzo','curso' : 'Análise e Desenvolvimento de Sistemas','turma' : '2026.2M'},
         {'nome' : 'João Gutierrez','curso' : 'Ciência da Computação','turma' : '2026.1T'},
         {'nome' : 'Marcela Correa','curso' : 'Engenharia de Software','turma': '2026.1N'},
     ]
 
-    return render(request, 'clientes\dados_clientes.html', { 'mensagem':titulo, 'clientesdb':lista_alunos })
+    return render(request, 'alunos\dados_alunos.html', { 'mensagem':titulo, 'clientesdb':lista_alunos })
