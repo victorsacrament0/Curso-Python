3d88869125e04a202109215ac0310e62bb47d14b
VictorSacramento <victorscosta@gmail.com>
2026-06-08 18:18:16 -0300

    sispsy

--- sis_psicologo/pacientes/models.py
+++ sis_psicologo/pacientes/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Pacientes(models.Model):
     queixa_choices = (
-        ('TDAH', 'Transtorno do Déficit de Atenção com Hiperatividade'),
-        ('D', 'Depressão'),
-        ('A', 'Ansiedade'),
-        ('TAG', 'Transtorno de Ansiedade Generalizada')
+        ('TDAH' , 'Transtorno do Déficit de Atenção com Hiperatividade'),
+        ('D' , 'Depressão'),
+        ('A' , 'Ansiedade'),
+        ('TAG' , 'Transtorno de Ansiedade Generalizada')
     )
     
     nome = models.CharField(max_length=255)
@@ -13,7 +13,7 @@ class Pacientes(models.Model):
     telefone = models.CharField(max_length=255, null=True, blank=True)
     foto = models.ImageField(upload_to='fotos')
     pagamento_em_dia = models.BooleanField(default=True)
-    queixa = models.CharField(max_length=4, choices=queixa_choices)
+    queixa = models.CharField(max_length=255, choices=queixa_choices)
 
     def __str__(self):
         return self.nome
